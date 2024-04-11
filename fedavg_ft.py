062a5c4f96fd1718e935d1c7a58c9e930dd66f98
RossChann <bc7b@mtmail.mtsu.edu>
2024-04-11 17:07:53 -0500

    1

--- fedavg_ft.py
+++ fedavg_ft.py
@@ -12,28 +12,11 @@ import os
 from utils import clear_cache_and_rec_usage
 from tensorflow.keras import backend as K
 import keras as keras
-import socket
-import pickle
-def send_weights(weights, host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        data = pickle.dumps(weights)
-        s.sendall(data)
 
-def receive_weights(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            data = b''
-            while True:
-                packet = conn.recv(4096)
-                if not packet:
-                    break
-                data += packet
-            weights = pickle.loads(data)
-    return weights
+global_accuracy_ft = 0
+global_accuracy_fet = 0
+global_accuracy_fetc = 0
+
 def full_training(
         model,
         ds_train,
@@ -43,7 +26,7 @@ def full_training(
         optim='sgd',
         lr=1e-4,
         weight_decay=5e-4,
-        epochs=12,
+        epochs=5,
         disable_random_id=False,
         save_model=False,
         save_txt=False,
@@ -160,8 +143,380 @@ def full_training(
     # sig_stop_handler(None, None)
 
 
+def elastic_training(
+        model,
+        model_name,
+        ds_train,
+        ds_test,
+        run_name,
+        logdir,
+        timing_info,
+        optim='sgd',
+        lr=1e-4,
+        weight_decay=5e-4,
+        epochs=5,
+        interval=5,
+        rho=0.533,
+        disable_random_id=False,
+        save_model=False,
+        save_txt=False,
+):
+    """Train with ElasticTrainer"""
+
+    def rho_for_backward_pass(rho):
+        return (rho - 1 / 3) * 3 / 2
+
+    t_dw, t_dy = profile_parser(
+        model,
+        model_name,
+        5,
+        'profile_extracted/' + timing_info,
+        draw_figure=False,
+    )
+    # np.savetxt('t_dy.out', t_dy)
+    # np.savetxt('t_dw.out', t_dw)
+    t_dy_q, t_dw_q, disco = downscale_t_dy_and_t_dw(t_dy, t_dw, Tq=1e3)
+    t_dy_q = np.flip(t_dy_q)
+    t_dw_q = np.flip(t_dw_q)
+
+    if optim == 'sgd':
+        decay_steps = len(tfds.as_numpy(ds_train)) * epochs
+
+        lr_schedule = tf.keras.experimental.CosineDecay(lr, decay_steps=decay_steps)
+        wd_schedule = tf.keras.experimental.CosineDecay(lr * weight_decay, decay_steps=decay_steps)
+        optimizer = tfa.optimizers.SGDW(learning_rate=lr_schedule, weight_decay=wd_schedule, momentum=0.9,
+                                        nesterov=False)
+    else:
+        optimizer = tf.keras.optimizers.Adam(lr)
+
+    loss_fn_cls = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    if disable_random_id:
+        runid = run_name
+    else:
+        runid = run_name + '_elastic_x' + str(np.random.randint(10000))
+
+    writer = tf.summary.create_file_writer(logdir + '/' + runid)
+    accuracy = tf.metrics.SparseCategoricalAccuracy()
+    cls_loss = tf.metrics.Mean()
+
+    print(f"RUNID: {runid}")
+
+    var_list = []
+    # initialze a gradient list in FL
+    gradients_list = []
+
+    def train_step(x, y):
+        with tf.GradientTape() as tape:
+            y_pred = model(x, training=True)
+            loss = loss_fn_cls(y, y_pred)
+        gradients = tape.gradient(loss, var_list)
+        optimizer.apply_gradients(zip(gradients, var_list))
+        accuracy(y, y_pred)
+        cls_loss(loss)
+
+    @tf.function
+    def test_step(x, y):
+        y_pred = model(x, training=False)
+        loss = loss_fn_cls(y, y_pred)
+        accuracy(y, y_pred)
+        cls_loss(loss)
+
+    @tf.function
+    def compute_dw(x, y):
+        with tf.GradientTape() as tape:
+            y_pred_0 = model(x, training=True)
+            loss_0 = loss_fn_cls(y, y_pred_0)
+        grad_0 = tape.gradient(loss_0, model.trainable_weights)
+        w_0 = [w.value() for w in model.trainable_weights]  # record initial weight values
+        optimizer.apply_gradients(zip(grad_0, model.trainable_weights))
+        w_1 = [w.value() for w in model.trainable_weights]  # record weight values after applying optimizer
+        dw_0 = [w_1_k - w_0_k for (w_0_k, w_1_k) in zip(w_0, w_1)]  # compute weight changes
+        with tf.GradientTape() as tape:
+            y_pred_1 = model(x, training=True)
+            loss_1 = loss_fn_cls(y, y_pred_1)
+        grad_1 = tape.gradient(loss_1, model.trainable_weights)
+        I = [tf.reduce_sum((grad_1_k * dw_0_k)) for (grad_1_k, dw_0_k) in zip(grad_1, dw_0)]
+        I = tf.convert_to_tensor(I)
+        I = I / tf.reduce_max(tf.abs(I))
+        # restore weights
+        for k, w in enumerate(model.trainable_weights):
+            w.assign(w_0[k])
+        return dw_0, I
+
+    training_step = 0
+    best_validation_acc = 0
+
+    total_time_0 = 0
+    total_time_1 = 0
+    for epoch in range(epochs):
+
+        t0 = time.time()
+        if epoch % interval == 0:
+            for x_probe, y_probe in ds_train.take(1):
+                dw, I = compute_dw(x_probe, y_probe)
+                I = -I.numpy()
+                I = np.flip(I)
+                # np.savetxt('importance.out', I)
+                rho_b = rho_for_backward_pass(rho)
+                max_importance, m = selection_DP(t_dy_q, t_dw_q, I, rho=rho_b * disco)
+                m = np.flip(m)
+                print("m:", m)
+                print("max importance:", max_importance)
+                print("%T_sel:", 100 * np.sum(np.maximum.accumulate(m) * t_dy + m * t_dw) / np.sum(t_dy + t_dw))
+                var_list = []
+                all_vars = model.trainable_weights
+                for k, m_k in enumerate(m):
+                    if tf.equal(m_k, 1):
+                        var_list.append(all_vars[k])
+                train_step_cpl = tf.function(train_step)
+        gradients_list = []
+        for x, y in tqdm(ds_train, desc=f'epoch {epoch + 1}/{epochs}', ascii=True):
+
+            training_step += 1
+            train_step_cpl(x, y)
+
+            if training_step % 200 == 0:
+                with writer.as_default():
+                    c_loss, acc = cls_loss.result(), accuracy.result()
+                    tf.summary.scalar('train/accuracy', acc, training_step)
+                    tf.summary.scalar('train/classification_loss', c_loss, training_step)
+                    tf.summary.scalar('train/learnig_rate', optimizer._decayed_lr('float32'), training_step)
+                    cls_loss.reset_states()
+                    accuracy.reset_states()
+                clear_cache_and_rec_usage()
+
+        cls_loss.reset_states()
+        accuracy.reset_states()
+
+        t1 = time.time()
+        print("per epoch time(s) excluding validation:", t1 - t0)
+        total_time_0 += (t1 - t0)
+
+        for x, y in ds_test:
+            test_step(x, y)
+
+        with writer.as_default():
+            tf.summary.scalar('test/classification_loss', cls_loss.result(), step=training_step)
+            tf.summary.scalar('test/accuracy', accuracy.result(), step=training_step)
+
+            if accuracy.result() > best_validation_acc:
+                best_validation_acc = accuracy.result()
+                if save_model:
+                    model.save_weights(os.path.join('saved_models', runid + '.tf'))
+                print("=================================")
+                print("acc: ", accuracy.result())
+                print("=================================")
+
+            cls_loss.reset_states()
+            accuracy.reset_states()
+
+        t2 = time.time()
+        print("per epoch time(s) including validation:", t2 - t0)
+        total_time_1 += (t2 - t0)
+
+    best_validation_acc = best_validation_acc.numpy() * 100
+    total_time_0 /= 3600
+    print('===============================================')
+    print('Training Type: ElasticTrainer')
+    print(f"Accuracy (%): {best_validation_acc:.2f}")
+    print(f"Time (h): {total_time_0:.2f}")
+    print('===============================================')
+    if save_txt:
+        np.savetxt(logdir + '/' + runid + '.txt', np.array([total_time_0, best_validation_acc]))
+
+    return model
 
 
+def elastic_training_updated(
+        I_G,
+        model,
+        model_name,
+        ds_train,
+        ds_test,
+        run_name,
+        logdir,
+        timing_info,
+        optim='sgd',
+        lr=1e-4,
+        weight_decay=5e-4,
+        epochs=5,
+        interval=5,
+        rho=0.533,
+        disable_random_id=False,
+        save_model=False,
+        save_txt=False,
+
+):
+    """Train with ElasticTrainer"""
+
+    def rho_for_backward_pass(rho):
+        return (rho - 1 / 3) * 3 / 2
+
+    t_dw, t_dy = profile_parser(
+        model,
+        model_name,
+        5,
+        'profile_extracted/' + timing_info,
+        draw_figure=False,
+    )
+    # np.savetxt('t_dy.out', t_dy)
+    # np.savetxt('t_dw.out', t_dw)
+    t_dy_q, t_dw_q, disco = downscale_t_dy_and_t_dw(t_dy, t_dw, Tq=1e3)
+    t_dy_q = np.flip(t_dy_q)
+    t_dw_q = np.flip(t_dw_q)
+
+    if optim == 'sgd':
+        decay_steps = len(tfds.as_numpy(ds_train)) * epochs
+
+        lr_schedule = tf.keras.experimental.CosineDecay(lr, decay_steps=decay_steps)
+        wd_schedule = tf.keras.experimental.CosineDecay(lr * weight_decay, decay_steps=decay_steps)
+        optimizer = tfa.optimizers.SGDW(learning_rate=lr_schedule, weight_decay=wd_schedule, momentum=0.9,
+                                        nesterov=False)
+    else:
+        optimizer = tf.keras.optimizers.Adam(lr)
+
+    loss_fn_cls = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    if disable_random_id:
+        runid = run_name
+    else:
+        runid = run_name + '_elastic_x' + str(np.random.randint(10000))
+
+    writer = tf.summary.create_file_writer(logdir + '/' + runid)
+    accuracy = tf.metrics.SparseCategoricalAccuracy()
+    cls_loss = tf.metrics.Mean()
+
+    print(f"RUNID: {runid}")
+
+    var_list = []
+    # initialze a gradient list in FL
+    gradients_list = []
+
+    def train_step(x, y):
+        with tf.GradientTape() as tape:
+            y_pred = model(x, training=True)
+            loss = loss_fn_cls(y, y_pred)
+        gradients = tape.gradient(loss, var_list)
+        optimizer.apply_gradients(zip(gradients, var_list))
+        accuracy(y, y_pred)
+        cls_loss(loss)
+
+    @tf.function
+    def test_step(x, y):
+        y_pred = model(x, training=False)
+        loss = loss_fn_cls(y, y_pred)
+        accuracy(y, y_pred)
+        cls_loss(loss)
+
+    @tf.function
+    def compute_dw(x, y, I_G):
+        with tf.GradientTape() as tape:
+            y_pred_0 = model(x, training=True)
+            loss_0 = loss_fn_cls(y, y_pred_0)
+        grad_0 = tape.gradient(loss_0, model.trainable_weights)
+        w_0 = [w.value() for w in model.trainable_weights]  # record initial weight values
+        optimizer.apply_gradients(zip(grad_0, model.trainable_weights))
+        w_1 = [w.value() for w in model.trainable_weights]  # record weight values after applying optimizer
+        dw_0 = [w_1_k - w_0_k for (w_0_k, w_1_k) in zip(w_0, w_1)]  # compute weight changes
+        with tf.GradientTape() as tape:
+            y_pred_1 = model(x, training=True)
+            loss_1 = loss_fn_cls(y, y_pred_1)
+        grad_1 = tape.gradient(loss_1, model.trainable_weights)
+        I = [tf.reduce_sum((grad_1_k * dw_0_k)) for (grad_1_k, dw_0_k) in zip(grad_1, dw_0)]
+        I = tf.convert_to_tensor(I)
+        I = I / tf.reduce_max(tf.abs(I))
+
+        # Compute the weighted average of I and I_G_selected
+        alpha = 0.6  # Weight for I_G_selected
+        I_weighted = alpha * I_G + (1 - alpha) * I
+        # restore weights
+        for k, w in enumerate(model.trainable_weights):
+            w.assign(w_0[k])
+        return dw_0, I_weighted
+
+    training_step = 0
+    best_validation_acc = 0
+
+    total_time_0 = 0
+    total_time_1 = 0
+    for epoch in range(epochs):
+
+        t0 = time.time()
+        if epoch % interval == 0:
+            for x_probe, y_probe in ds_train.take(1):
+                dw, I = compute_dw(x_probe, y_probe, I_G)
+                I = -I.numpy()
+                I = np.flip(I)
+                # np.savetxt('importance.out', I)
+                rho_b = rho_for_backward_pass(rho)
+                max_importance, m = selection_DP(t_dy_q, t_dw_q, I, rho=rho_b * disco)
+                m = np.flip(m)
+                print("m:", m)
+                print("max importance:", max_importance)
+                print("%T_sel:", 100 * np.sum(np.maximum.accumulate(m) * t_dy + m * t_dw) / np.sum(t_dy + t_dw))
+                var_list = []
+                all_vars = model.trainable_weights
+                for k, m_k in enumerate(m):
+                    if tf.equal(m_k, 1):
+                        var_list.append(all_vars[k])
+                train_step_cpl = tf.function(train_step)
+        gradients_list = []
+        for x, y in tqdm(ds_train, desc=f'epoch {epoch + 1}/{epochs}', ascii=True):
+
+            training_step += 1
+            train_step_cpl(x, y)
+
+            if training_step % 200 == 0:
+                with writer.as_default():
+                    c_loss, acc = cls_loss.result(), accuracy.result()
+                    tf.summary.scalar('train/accuracy', acc, training_step)
+                    tf.summary.scalar('train/classification_loss', c_loss, training_step)
+                    tf.summary.scalar('train/learnig_rate', optimizer._decayed_lr('float32'), training_step)
+                    cls_loss.reset_states()
+                    accuracy.reset_states()
+                clear_cache_and_rec_usage()
+
+        cls_loss.reset_states()
+        accuracy.reset_states()
+
+        t1 = time.time()
+        print("per epoch time(s) excluding validation:", t1 - t0)
+        total_time_0 += (t1 - t0)
+
+        for x, y in ds_test:
+            test_step(x, y)
+
+        with writer.as_default():
+            tf.summary.scalar('test/classification_loss', cls_loss.result(), step=training_step)
+            tf.summary.scalar('test/accuracy', accuracy.result(), step=training_step)
+
+            if accuracy.result() > best_validation_acc:
+                best_validation_acc = accuracy.result()
+                if save_model:
+                    model.save_weights(os.path.join('saved_models', runid + '.tf'))
+                print("=================================")
+                print("acc: ", accuracy.result())
+                print("=================================")
+
+            cls_loss.reset_states()
+            accuracy.reset_states()
+
+        t2 = time.time()
+        print("per epoch time(s) including validation:", t2 - t0)
+        total_time_1 += (t2 - t0)
+
+    best_validation_acc = best_validation_acc.numpy() * 100
+    total_time_0 /= 3600
+    print('===============================================')
+    print('Training Type: ElasticTrainer')
+    print(f"Accuracy (%): {best_validation_acc:.2f}")
+    print(f"Time (h): {total_time_0:.2f}")
+    print('===============================================')
+    if save_txt:
+        np.savetxt(logdir + '/' + runid + '.txt', np.array([total_time_0, best_validation_acc]))
+
+    return model
 
 
 def federated_elastic_training_advanced(client_datasets, ds_test, model_type='resnet50', global_epochs=4,
@@ -194,36 +549,44 @@ def federated_elastic_training_advanced(client_datasets, ds_test, model_type='re
                                           num_classes=num_classes)  # Load global model
 
     for global_epoch in range(global_epochs):
-        client_host='192.168.1.243'
-        client_port='8001'
-        server_host='192.168.1.248'
-        server_port='8000'
         print(f"Global Epoch {global_epoch + 1}/{global_epochs}")
         client_weights = []
+        compute_client_weights = []
+        if global_epoch == 0:
+            for client_id, ds_train in enumerate(client_datasets):
+                print(f"Training on client {client_id + 1}/{len(client_datasets)}")
+                client_model = port_pretrained_models(model_type=model_type, input_shape=input_shape,
+                                                      num_classes=num_classes)  # Create model for each client and initailze the weights
+                client_model=full_training(client_model, model_name, ds_train, ds_test, run_name='auto',
+                                                logdir='auto', timing_info=timing_info, optim='sgd', lr=1e-4,
+                                                weight_decay=5e-4, epochs=5, rho=0.533,
+                                                disable_random_id=True, save_model=False, save_txt=False)
+                client_model = elastic_training(client_model, model_name, ds_train, ds_test, run_name='auto',
+                                                logdir='auto', timing_info=timing_info, optim='sgd', lr=1e-4,
+                                                weight_decay=5e-4, epochs=5, interval=5, rho=0.533,
+                                                disable_random_id=True, save_model=False, save_txt=False)  # train
 
-        print("Training on device 1 (server)")
-        device1_model = port_pretrained_models(model_type=model_type, input_shape=input_shape,
-                                               num_classes=num_classes)
-        device1_model.set_weights(global_model.get_weights())
-        device1_model = full_training(device1_model, client_datasets[0], ds_test, run_name='auto', logdir='auto',
-                                      optim='sgd', lr=1e-4, weight_decay=5e-4, epochs=12, disable_random_id=False,
-                                      save_model=False, save_txt=False)
-        client_weights.append(device1_model.get_weights())
+                client_model=full_training(client_model,ds_train,ds_test,run_name='auto',logdir='auto',optim='sgd',lr=1e-4,weight_decay=5e-4,epochs=5)
 
-        print("Training on device 2 (client)")
-        device2_model = port_pretrained_models(model_type=model_type, input_shape=input_shape,
-                                               num_classes=num_classes)
-        device2_model.set_weights(receive_weights(client_host, client_port))  # 从服务器接收全局模型权重
-        device2_model = full_training(device2_model, client_datasets[1], ds_test, run_name='auto', logdir='auto',
-                                      optim='sgd', lr=1e-4, weight_decay=5e-4, epochs=12, disable_random_id=False,
-                                      save_model=False, save_txt=False)
-        send_weights(device2_model.get_weights(), server_host, server_port)
+                compute_client_weights.append(client_model.get_weights())  # 320
+            compute_weights = aggregate_weights(compute_client_weights)  # 320
 
-        client_weights.append(receive_weights(server_host, server_port))  # 服务器接收客户端模型权重
-        compute_weights = aggregate_weights(client_weights)
-        update_global_model(global_model, compute_weights)
+            update_global_model(global_model, compute_weights)  # 320->320
 
-        show_results(global_model)
+            show_results(global_model)
+
+        else:
+            for client_id, ds_train in enumerate(client_datasets):
+                print(f"Training on client {client_id + 1}/{len(client_datasets)}")
+                client_model.set_weights(global_model.get_weights())
+                client_model = full_training(client_model,ds_train,ds_test,run_name='auto',logdir='auto',optim='sgd',lr=1e-4,weight_decay=5e-4,epochs=5)
+                compute_client_weights.append(client_model.get_weights())  # 320
+            compute_weights = aggregate_weights(compute_client_weights)  # 320
+
+            update_global_model(global_model, compute_weights)  # 320->320
+
+
+            show_results(global_model)
 
     return global_model
 
